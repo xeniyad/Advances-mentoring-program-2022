6b464bf4d177a63e579557c8a535e4f1798591d6
xeniya <xeniya_denissova@epam.com>
2022-09-07 16:49:57 +0600

    missed files add

---
+++ CartingService/CartingService/CartingContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Carting.DL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CatalogService.Infrastructure;
+
+public class CartingContextFactory : IDesignTimeDbContextFactory<CartingContext>
+{
+    public CartingContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<CartingContext>();
+        optionsBuilder.UseSqlServer("Server=localhost\\SQLEXPRESS;Database=carting;Trusted_Connection=True;");
+
+        return new CartingContext(optionsBuilder.Options);
+    }
+}
---
+++ CartingService/CartingService/DL/CartingContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Data.Common;
+using System.Reflection;
+
+namespace Carting.DL
+{
+    public class CartingContext : DbContext, IDisposable
+    {
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public string DbPath { get; }
+
+        public CartingContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
